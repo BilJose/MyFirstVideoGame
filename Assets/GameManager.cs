99eac13c2e69c78128e30f04d82d798748b42230
BilJose <poollooo.1995@gmail.com>
2019-01-23 21:44:16 -0600

    adding obstacles to the game and movements.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public void GameOver()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+            //Restart();
+
+        }
+
+        void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
